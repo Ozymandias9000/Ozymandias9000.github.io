d213e1b1a1f181dcdb73db31dbe04204a7f3932b
Nick <nick.murphy@fansided.com>
2021-01-22 14:33:23 -0700

    Add analytics

--- src/App.svelte
+++ src/App.svelte
@@ -6,8 +6,12 @@
 <svelte:head>
   <link
     href="https://fonts.googleapis.com/css2?family=IBM+Plex+Serif:ital,wght@0,200;0,600;1,200&display=swap"
-    rel="stylesheet" />
-  <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@300;600&display=swap" rel="stylesheet" />
-</svelte:head>
+    rel="stylesheet"
+  />
+  <link
+    href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@300;600&display=swap"
+    rel="stylesheet"
+  /><script data-goatcounter="https://ozy.goatcounter.com/count" async src="//gc.zgo.at/count.js"></script></svelte:head
+>
 
 <FrontPage />
