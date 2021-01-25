f2dbc824135c5d26590a3e862acdc1419ac686c2
Nick <nick.murphy@fansided.com>
2021-01-25 14:09:52 -0700

    Add background to contact

--- src/components/ContactForm.svelte
+++ src/components/ContactForm.svelte
@@ -1,20 +1,26 @@
 <div class="relative bottom-52 px-40 pt-80 flex flex-col items-center justify-center">
-  <form action="https://app.99inbound.com/api/e/UIOmDCjj" method="POST" target="_blank" class="flex flex-col ">
-    <input type="text" name="name" placeholder="Your Name" required class="mb-2 p-1 prose md:w-full w-80 m-auto" />
-    <input type="email" name="email" placeholder="Your Email" required class="mb-2 p-1 prose md:w-full w-80 m-auto" />
+  <!-- <div class="transform -skew-x-12 -rotate-3 bg-royalblue-800 rounded-2xl shadow-2xl svgBox"></div> -->
+  <form
+    action="https://app.99inbound.com/api/e/UIOmDCjj"
+    method="POST"
+    target="_blank"
+    class="flex flex-col bg-royalblue-800 rounded-2xl p-6 mb-4 border-2 border-asparagus-400 md:w-auto w-76"
+  >
+    <input type="text" name="name" placeholder="Your Name" required class="mb-2 p-1 prose md:w-full w-64 m-auto" />
+    <input type="email" name="email" placeholder="Your Email" required class="mb-2 p-1 prose md:w-full w-64 m-auto" />
 
     <textarea
       name="message"
       placeholder="Message here..."
       cols="50"
       rows="10"
-      class="mb-2 p-1 prose md:w-full w-80 m-auto"
+      class="mb-2 p-1 prose md:w-full w-64 m-auto"
     />
 
     <div style="position: absolute; left: -5000px;">
       <input type="checkbox" name="spectacular_brown_striped_chandelier" value="1" tabindex="-1" autocomplete="no" />
     </div>
 
-    <button class="p-1 prose rounded-xl hover:text-gray-400">Submit</button>
+    <button class="p-1 prose rounded-xl hover:text-gray-200 text-white bg-asparagus-400">Submit</button>
   </form>
 </div>
